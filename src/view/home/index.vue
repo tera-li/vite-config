03e9d9e803dce775850e5bbcc50a4a6eb2f279c5
ljl <776745393@qq.com>
2021-07-04 15:20:18 +0800

    address

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -2,13 +2,13 @@
     <div>
         <p>Home</p>
         <img :src="imgUrl" width="100" height="100"/>
-        <p>This is to JSON ,From {{address}}</p>
+        <!-- <p>This is to JSON ,From {{address}}</p> -->
     </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import imgUrl from '@/assets/home.png'
-import { address } from '@/assets/home.json'
+// import { address } from '@/assets/home.json'
 
 export default defineComponent({
     name: 'Home',
@@ -16,7 +16,7 @@ export default defineComponent({
         // const imgQ = new URL('./.png', import.meta.url)
         // console.log(imgQ);
 
-        return {imgUrl,address}
+        return {imgUrl}
     },
 })
 </script>
