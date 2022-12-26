f133cbc2e5f24b0536ba9593cd1cc1f8a20cd109
surprise <776745393@qq.com>
2022-12-26 16:22:20 +0800

    refactor(vite)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
+console.log(process.env.NODE_ENV);
+console.log(process.env.VITE_SOME_KEY);
+
 export default defineConfig({
+  // 公共路径，静态资源链接
+  base: "./",
   plugins: [vue()],
-})
+});
